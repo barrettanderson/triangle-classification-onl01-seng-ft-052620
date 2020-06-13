2f635c39a943ae111bdb3c062b9324210d829546
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-13 03:15:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,12 +5,12 @@
 #     @triangle_sides << side_2
 #     @triangle_sides << side_3
 #   end
-# 
+#
 #   def valid?
 #     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
 #     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
 #     sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-# 
+#
 #     if (@triangle_sides.none? {|side| side <= 0}) &&
 #       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
 #       return true
@@ -18,7 +18,7 @@
 #       return false
 #     end
 #   end
-# 
+#
 #   def kind
 #     if valid?
 #       if @triangle_sides.uniq.length == 1
@@ -33,9 +33,9 @@
 #     end
 #   end
 # end
-# 
+#
 # class TriangleError < StandardError
-# 
+#
 # end
 
 class Triangle
