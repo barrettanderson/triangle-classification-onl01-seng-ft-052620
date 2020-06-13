df05c107896bd40a8d97c28e36d7b1631baa0428
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-13 03:12:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,16 +9,17 @@ class Triangle
   end
 
   def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+   sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+   sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+   sum_two_three = @triangle_sides[1] + @triangle_sides[2]
 
-    if (@triangle_sides.none? {|side| side <=0}) && (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
+   if (@triangle_sides.none? {|side| side <= 0}) &&
+     (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+     return true
+   else
+     return false
+   end
+ end
 
   def kind
      if valid?
